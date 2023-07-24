b2b42099c3df783e5549b2900cd2f0b3546df843
Resham Chandak <63545418+11910417@users.noreply.github.com>
2023-07-24 19:37:01 +0530

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,8 +3,10 @@ import unittest
 class TestTranslator(unittest.TestCase):
     def test1_english_to_french(self):
         self.assertEqual(english_to_french("Hello"),"Bonjour")
+        self.assertNotEqual(english_to_french("I am Resham Chandak"),"Jius Resham Chandak")
     
     def test2_french_to_english(self):
         self.assertEqual(french_to_english("Bonjour"),"Hello")
+        self.assertNotEqual(french_to_english("Jius Resham Chandak"),"I am Resham Chandak")
 
 unittest.main()
